cc44330e1e7987be1903a4d68f114cd823929fa5
arkham <jesm430@gmail.com>
2025-11-28 17:35:58 -0700

    Correccion del resetDB

--- resetDB.js
+++ resetDB.js
@@ -1,23 +1,23 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2/promise"); //solicita el modulo de mysql que tiene express
 const fs = require("fs");
 
-async function resetDatabase() {
+async function resetDatabase() { //crea la funcion resetDatabase la cual funciona para leer el archivo llamado dbhive.sql y ejecutarlo en mysql
     try {
-        const connection = await mysql.createConnection({
+        const connection = await mysql.createConnection({ //esta parte funciona para acceder a mysql ya q es la informacion que solicita al acceder
         host: "localhost",
         user: "root", 
         password: "190906",
         multipleStatements : true
         });
 
-        const sql = fs.readFileSync("dbhive.sql", "utf8");
+        const sql = fs.readFileSync("dbhive.sql", "utf8"); //es aqui donde se lee el archivo
 
-        await connection.query(sql);
+        await connection.query(sql); //aqui hace la conexion a mysql
 
-        console.log("Se reinicio correctamente la fokin BD");
-        await connection.end();
+        console.log("Se reinicio correctamente la DB");
+        await connection.end(); //cierra la conexion despues de que la ejecucion sea correcta 
     } catch (err) {
-        console.error("Ubo un error al reiniciar la bd: ", err);
+        console.error("Ubo un error al reiniciar la bd: ", err); //si llega a haber algun error, el catch lo atrapa y muestra el error
     }
 }
 
